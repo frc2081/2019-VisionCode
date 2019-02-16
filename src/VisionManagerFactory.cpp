07582affbd6403dc1480a15ff05f4ab5fe29810c
jcooley-hylasoft <j.cooley@hylasoftusa.com>
2019-02-16 02:16:23 +0000

    Changes vision manager to use a vector of filters.

--- src/VisionManagerFactory.cpp
+++ src/VisionManagerFactory.cpp
@@ -9,11 +9,13 @@
 #include "TargetFilter.h"
 #include "TextDisplay.h"
 
+using namespace std;
+
 namespace Icarus
 {
 	VisionManager * VisionManagerFactory::BuildManager()
 	{
-		return new VisionManager(Config(), Source(), Sink(), Filter());
+		return new VisionManager(Config(), Source(), Sink(), Filters());
 	}
 
 	VisionSource * VisionManagerFactory::BuildSource()
@@ -48,9 +50,12 @@ namespace Icarus
     }
 	}
 
-  VisionFilter* VisionManagerFactory::BuildFilter()
+  vector<VisionFilter*>* VisionManagerFactory::BuildFilters()
   {
-    return new TargetFilter();
+    return new vector<VisionFilter*>
+    {
+       new TargetFilter()
+    };
   }
 
 	VisionManagerFactory::VisionManagerFactory(int argc, char ** argv)
@@ -65,7 +70,7 @@ namespace Icarus
 		_manager = NULL;
 		_source = NULL;
 		_sink = NULL;
-    _filter = NULL;
+    _filters = NULL;
 	}
 
 
@@ -76,7 +81,14 @@ namespace Icarus
 		FACT_CLEAN(_manager);
 		FACT_CLEAN(_source);
 		FACT_CLEAN(_sink);
-    FACT_CLEAN(_filter);
+
+    if (_filters != NULL)
+    {
+      for(auto i = _filters->begin(); i != _filters->end(); i++)
+        delete *i;
+
+      delete _filters;
+    }
 	}
 
 	VisionManager * VisionManagerFactory::Manager()
@@ -104,12 +116,12 @@ namespace Icarus
 		return _sink;
 	}
 
-  VisionFilter* VisionManagerFactory::Filter()
+  vector<VisionFilter*>* VisionManagerFactory::Filters()
   {
-    if (_filter == NULL)
-      _filter = BuildFilter();
+    if (_filters == NULL)
+      _filters = BuildFilters();
 
-    return _filter;
+    return _filters;
   }
 
 	VisionConfiguration * VisionManagerFactory::Config()
