30d12e1713e7cebfafcbed41de48615a7dd19ad4
jcooley-hylasoft <j.cooley@hylasoftusa.com>
2019-02-16 03:40:56 +0000

    Implements the majority of the TargetPairFilter.

--- include/TargetPairFilter.h
+++ include/TargetPairFilter.h
@@ -4,6 +4,11 @@ namespace Icarus
 {
   class TargetPairFilter : public VisionFilter
   {
+    protected:
+      static bool IsLeftOf(Contour a, Contour b);
+      void SortContours(std::vector<Contour>* contours);
+      bool IsRightLeaning(Contour contour);
+
     public:
       TargetPairFilter();
       virtual ~TargetPairFilter();
--- src/TargetPairFilter.cpp
+++ src/TargetPairFilter.cpp
@@ -13,7 +13,49 @@ namespace Icarus
   {
   }
 
+  bool TargetPairFilter::IsLeftOf(Contour a, Contour b)
+  {
+    return a.at(0).x < b.at(0).x;
+  }
+
+  void TargetPairFilter::SortContours(vector<Contour>* contours)
+  {
+    sort(contours->begin(), contours->end(), TargetPairFilter::IsLeftOf);
+  }
+
+  bool TargetPairFilter::IsRightLeaning(Contour contour)
+  {
+    // TODO: Implement this.
+
+    // Correct Algorithm:
+    // If the rightmost point is above the leftmost point, it is right-leaning.
+    return true;
+  }
+
   void TargetPairFilter::Filter(ImageData* data)
   {
+    vector<Contour>* contours = data->GetContours();
+    SortContours(contours);
+
+    // Find the left-most right-leaning contour.
+    // Removes all before it.
+    auto i = contours->begin();
+    while(i != contours->end())
+      if(IsRightLeaning(*i))
+        break;
+      else
+        i = contours->erase(i);
+    
+    // If fewer than left, right, and extra exist.
+    // Just return.
+    if (contours->size() < 3)
+      return;
+
+    // Increment twice to preserve left and right.
+    ++i; i++;
+
+    // Remove all after.
+    while(i != contours->end())
+      i = contours->erase(i);
   }
 }
