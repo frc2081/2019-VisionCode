a7d7a8cbeee56d4c001021d9e4b08a4bde2906b9
jcooley-hylasoft <j.cooley@hylasoftusa.com>
2019-02-19 03:11:20 +0000

    Adds test source.

---
+++ src/TestSource.cpp
@@ -0,0 +1,41 @@
+#include "stdafx.h"
+#include "TestSource.h"
+
+using namespace std;
+using namespace grip;
+using namespace cv;
+
+namespace Icarus
+{
+	void TestSource::Init()
+	{
+		_pipeline = new GripPipeline(_config);
+    ReadTestImage();
+	}
+
+	void TestSource::Clean()
+	{
+		delete _pipeline;
+	}
+
+	void TestSource::Source(ImageData * data)
+	{	
+		data->SetImageData(&_testImage);
+		data->SetContours(_contours);
+	}
+
+  void TestSource::ReadTestImage()
+  {
+    const char* testImageName = "SimulatedTargets.svg";
+    string testImage = _config->GetShareDir() + "/" + testImageName;
+
+    _testImage = imread(testImage);
+    _pipeline->Process(_testImage);
+    _contours = _pipeline->GetFilterContoursOutput();
+  }
+
+	TestSource::TestSource(VisionConfiguration * config)
+	{
+		_config = config;
+	}
+}
